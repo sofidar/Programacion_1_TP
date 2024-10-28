048c42393961ad29b68bf770bb26d5271304bc19
valsalama <vsalama@uade.edu.ar>
2024-10-28 17:19:53 -0300

    Update validaciones.py

--- validaciones.py
+++ validaciones.py
@@ -2,7 +2,7 @@ def es_entero(valor):
     return valor.isdigit() and int(valor) > 0
 
 def opcion_valida_menu(opcion):
-    return opcion.isdigit() and 1 <= int(opcion) <= 9
+    return opcion.isdigit() and 1 <= int(opcion) <= 11
 
 def cadena_vacia(cadena):
     if len(cadena) == 0:
