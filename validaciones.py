ea0a31fbfbfe674cd63e0ee1272939761d7e3b67
lolaescalass <lolaescalaspralong@gmail.com>
2024-10-29 23:26:42 -0300

    Update validaciones.py
    un numero natural por excepciones

--- validaciones.py
+++ validaciones.py
@@ -1,5 +1,5 @@
-def es_entero(valor):
-    return valor.isdigit() and int(valor) > 0
+def es_entero(numero):
+    return numero.isdigit() and int(numero) > 0
 
 def opcion_valida_menu(opcion):
     return opcion.isdigit() and 1 <= int(opcion) <= 11
@@ -9,3 +9,23 @@ def cadena_vacia(cadena):
         return True
     else:
         return False
+    
+def es_natural():
+    # Verifica que numero sea natural con excepciones
+    while True:
+        try:
+                numero=int(input())
+                division=10/numero
+        except ValueError:
+                print("No se puede ingresar texto, o numeros decimales")
+                continue
+        except ZeroDivisionError:
+                print("No se puede ingresar 0")
+                continue
+        try:
+            assert numero>0
+        except AssertionError:
+            print("No se pueden ingresar números negativos")
+            continue
+        break
+    return numero
